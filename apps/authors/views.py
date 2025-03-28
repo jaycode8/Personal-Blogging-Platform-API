fa7bffdcf2dce74f5d8743517d9bdaafa67ee9bb
Oliver Mutuku <olivermulei001@gmail.com>
2025-03-28 14:00:07 +0300

    Just finished authors' API to GET and POST

--- apps/authors/views.py
+++ apps/authors/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import AuthorSerializer
+from .models import Author
 
-# Create your views here.
+
+@api_view(["GET", "POST"])
+def author_list(request):
+    if request.method == "GET":
+        # get all authors, serialize them, return json
+        authors = Author.objects.all()
+        serializer = AuthorSerializer(authors, many=True)
+        return JsonResponse({"authors": serializer.data})
+    
+    if request.method == "POST":
+        serializer = AuthorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
